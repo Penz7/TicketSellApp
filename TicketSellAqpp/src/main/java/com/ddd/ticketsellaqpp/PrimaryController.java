cec64eccc4bf0b67e5768655d735c917b3b6aeba
Penz7 <tandatkt002@gmail.com>
2023-03-18 20:15:57 +0700

    thêm đăng xuất

--- TicketSellAqpp/src/main/java/com/ddd/ticketsellaqpp/PrimaryController.java
+++ TicketSellAqpp/src/main/java/com/ddd/ticketsellaqpp/PrimaryController.java
@@ -1,7 +1,10 @@
 package com.ddd.ticketsellaqpp;
 
 import java.io.IOException;
+import java.util.Optional;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class PrimaryController {
 
@@ -9,4 +12,21 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    @FXML
+          // đăng xuất
+    private void setSignOut() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Đăng xuất");
+        alert.setHeaderText("Bạn có muốn đăng xuất không ?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get().equals(ButtonType.OK)) {
+            try {
+                App.currentStaff = null;
+                App.setRoot("sign-in");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
 }
