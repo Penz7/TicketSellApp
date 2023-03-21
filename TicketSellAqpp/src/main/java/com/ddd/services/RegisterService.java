abbbf80ceca0409f7ac85fed9a12c8916c8fd3cf
Penz7 <tandatkt002@gmail.com>
2023-03-21 11:47:32 +0700

    cập nhật trang đăng ký

---
+++ TicketSellAqpp/src/main/java/com/ddd/services/RegisterService.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ddd.services;
+
+import com.ddd.utils.JdbcUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.sql.Date;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author admin
+ */
+public class RegisterService {
+
+    public boolean addAccount(String fullName, String idCard, String phoneNumber, String username, String password, String address) {
+    LocalDate currentDate = LocalDate.now();
+    String sql = "INSERT INTO user(user_fullname, user_id_card, user_phone_number, user_date_join, username, password, user_address) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    Connection conn = null;
+
+    try {
+        conn = JdbcUtils.getConn();
+        conn.setAutoCommit(false);
+        PreparedStatement statement = conn.prepareStatement(sql);
+
+        statement.setString(1, fullName);
+        statement.setString(2, idCard);
+        statement.setString(3, phoneNumber);
+        statement.setDate(4, Date.valueOf(currentDate));
+        statement.setString(5, username);
+        statement.setString(6, password);
+        statement.setString(7, address);
+
+        int rowsInserted = statement.executeUpdate();
+
+        if (rowsInserted > 0) {
+            conn.commit();
+            return true;
+        } else {
+            conn.rollback();
+            return false;
+        }
+    } catch (SQLException e) {
+        if (conn != null) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        // Handle the error and log it
+        e.printStackTrace();
+        return false;
+    } finally {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+    
+    public boolean checkAccountUsername(String username) {
+    String sql = "SELECT COUNT(*) FROM user WHERE username = ?";
+    try (Connection conn = JdbcUtils.getConn(); PreparedStatement statement = conn.prepareStatement(sql)) {
+        statement.setString(1, username);
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+                int count = resultSet.getInt(1);
+                return (count == 0); // trả về true nếu username mới không trùng với username trong database
+            }
+        }
+    } catch (SQLException e) {
+        // Xử lý lỗi và ghi log
+        e.printStackTrace();
+    }
+    return false;
+}
+}
--- TicketSellAqpp/src/main/java/com/ddd/ticketsellaqpp/RegisterController.java
+++ TicketSellAqpp/src/main/java/com/ddd/ticketsellaqpp/RegisterController.java
@@ -4,15 +4,87 @@
  */
 package com.ddd.ticketsellaqpp;
 
+import com.ddd.services.RegisterService;
+import com.ddd.utils.MessageBox;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author admin
  */
-public class RegisterController {
-      @FXML
+public class RegisterController implements Initializable {
+
+    RegisterService registerService = new RegisterService();
+
+    @FXML
     private Button btnRegis;
 
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserfullname;
+
+    @FXML
+    private TextField txtUserphonenumber;
+
+    @FXML
+    private TextField txtUseraddress;
+
+    @FXML
+    private TextField txtUseridcard;
+
+    @FXML
+    private TextField txtPassword2;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.btnRegis.setOnMouseClicked(t -> {
+            handleRegister();
+        });
+    }
+
+    public void clearFields() {
+        this.txtUserfullname.setText("");
+        this.txtPassword.setText("");
+        this.txtPassword2.setText("");
+        this.txtUseraddress.setText("");
+        this.txtUseridcard.setText("");
+        this.txtUserphonenumber.setText("");
+        this.txtUsername.setText("");
+    }
+
+    @FXML
+    private void handleRegister() {
+        String fullName = txtUserfullname.getText();
+        String idCard = txtUseridcard.getText();
+        String phoneNumber = txtUserphonenumber.getText();
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+        String address = txtUseraddress.getText();
+
+        boolean isUsernameTaken = registerService.checkAccountUsername(username);
+        if (isUsernameTaken) {
+            boolean isSuccess = registerService.addAccount(fullName, idCard, phoneNumber, username, password, address);
+            if (isSuccess) {
+                MessageBox.getBox("Đăng ký tài khoản", "Đăng ký thành công !!", Alert.AlertType.CONFIRMATION).show();
+                clearFields(); // Xóa dữ liệu trên form đăng ký
+            } else {
+                MessageBox.getBox("Đăng ký tài khoản", "Đăng ký thất bại !!", Alert.AlertType.ERROR).show();
+            }
+        } else {
+            MessageBox.getBox("Đăng ký tài khoản", "Tên tài khoản đã bị trùng!!", Alert.AlertType.ERROR).show();
+        }
+
+    }
 }
